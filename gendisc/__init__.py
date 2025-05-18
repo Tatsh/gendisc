c8be4ac3d6fa4707b7df522e9811120ea4b0754c
Andrew Udvare <audvare@gmail.com>
2025-05-17 22:23:22 -0400

    bump: version 0.0.0 → 0.0.1

--- gendisc/__init__.py
+++ gendisc/__init__.py
@@ -4,4 +4,4 @@ from __future__ import annotations
 from .utils import DirectorySplitter, Point, write_spiral_text_svg
 
 __all__ = ('DirectorySplitter', 'Point', 'write_spiral_text_svg')
-__version__ = '0.0.0'
+__version__ = '0.0.1'
