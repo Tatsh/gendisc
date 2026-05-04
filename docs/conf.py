a4b605f55363f9ba8bed159e46e9844beb59d8c3
Andrew Udvare <audvare@gmail.com>
2026-05-04 19:02:45 -0400

    docs.conf: add anyio, rich, wakepy to intersphinx mapping
    Signed-off-by: Andrew Udvare <audvare@gmail.com>

--- docs/conf.py
+++ docs/conf.py
@@ -75,9 +75,12 @@ html_theme_options = {
     'toc_title_is_page_title': True
 }
 intersphinx_mapping = {
+    'anyio': ('https://anyio.readthedocs.io/en/stable/', None),
     'bascom': ('https://bascom.readthedocs.io/en/latest/', None),
     'click': ('https://click.palletsprojects.com/en/latest/', None),
     'jinja2': ('https://jinja.palletsprojects.com/en/latest/', None),
     'python': ('https://docs.python.org/3', None),
-    'typing-extensions': ('https://typing-extensions.readthedocs.io/en/latest/', None)
+    'rich': ('https://rich.readthedocs.io/en/stable/', None),
+    'typing-extensions': ('https://typing-extensions.readthedocs.io/en/latest/', None),
+    'wakepy': ('https://wakepy.readthedocs.io/stable/', None)
 }
